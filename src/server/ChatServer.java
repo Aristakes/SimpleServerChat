598deda93fd95d89edc85cab1ce1736f9280b0cd
Aristakes_prog <arissirkin@gmail.com>
2019-12-30 17:07:42 +0100

    Initial commit

---
+++ src/server/ChatServer.java
@@ -0,0 +1,66 @@
+package server;
+
+import network.TCPConnection;
+import network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+
+    public static void main(String[] args){
+
+        new ChatServer();
+
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer(){
+        System.out.println("Server running....");
+        try(ServerSocket serverSocket = new ServerSocket(8189)){
+            while(true){
+                try{
+                    new TCPConnection(this , serverSocket.accept());
+                }catch (IOException e){
+                    System.out.println("TCPConnection exception " + e);
+                }
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendAllConnetion("Client connected : " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendAllConnetion(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendAllConnetion("Client disconnected : " + tcpConnection);
+
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception : " + e);
+    }
+
+    private void sendAllConnetion(String value){
+        System.out.println(value);
+        for (int i = 0; i < connections.size(); i++) {
+            connections.get(i).sendString(value);
+        }
+    }
+}
